19acf330900bc90ef420e99ea8ae762dea7d36ec
enzoberreur <enzo.berreur@gmail.com>
2025-10-15 10:47:57 +0200

    data processing

---
+++ data_processing.py
@@ -0,0 +1,36 @@
+import pandas as pd
+from datasets import Dataset
+import os
+from dotenv import load_dotenv
+
+
+gcs_uri = f"gs://bucketllmops/yoda_sentences.csv" 
+df = pd.read_csv(gcs_uri)
+
+hf_dataset = Dataset.from_pandas(df)
+
+def format_dataset(examples):
+    if isinstance(examples["prompt"], list):
+        output_texts = []
+        for i in range(len(examples["prompt"])):
+            converted_sample = [
+                {"role": "user", "content": examples["prompt"][i]},
+                {"role": "assistant", "content": examples["completion"][i]},
+            ]
+            output_texts.append(converted_sample)
+        return {'messages': output_texts}
+    else:
+        converted_sample = [
+            {"role": "user", "content": examples["prompt"]},
+            {"role": "assistant", "content": examples["completion"]},
+        ]
+        return {'messages': converted_sample}
+
+dataset = hf_dataset.rename_column("sentence", "prompt")
+dataset = dataset.rename_column("translation_extra", "completion")
+dataset = dataset.map(format_dataset)
+dataset = dataset.remove_columns(['prompt', 'completion', 'translation'])
+messages = dataset[0]['messages']
+messages
+
+print(messages)
