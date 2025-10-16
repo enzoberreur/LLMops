983866cc84d165ab6083c0306e9004f327cafe1c
enzoberreur <enzo.berreur@gmail.com>
2025-10-16 16:18:34 +0200

    session2

--- scripts/check_gcp.py
+++ scripts/check_gcp.py
@@ -2,7 +2,6 @@ import os
 from pathlib import Path
 from dotenv import load_dotenv
 
-# Charger les variables d'environnement
 load_dotenv(Path(__file__).parent.parent / ".env")
 
 project = os.getenv("GCP_PROJECT_ID")
@@ -15,14 +14,12 @@ print(f"[CHECK] GCP_BUCKET_NAME: {bucket}")
 
 from google.cloud import aiplatform, storage
 
-# Vérifier l'initialisation du client Vertex AI
 try:
 	aiplatform.init(project=project, location=region)
 	print("[CHECK] Vertex AI client initialized successfully.")
 except Exception as e:
 	print(f"[FAIL] Vertex AI client initialization failed: {e}")
 
-# Vérifier l'accès au bucket GCS
 try:
 	storage_client = storage.Client(project=project)
 	blobs = list(storage_client.list_blobs(bucket))
--- scripts/pipeline_runner.py
+++ scripts/pipeline_runner.py
@@ -13,32 +13,27 @@ load_dotenv()
 PROJECT_ID = os.getenv("GCP_PROJECT_ID")
 REGION = os.getenv("GCP_REGION")
 PIPELINE_ROOT = os.getenv("PIPELINE_ROOT", f"gs://{os.getenv('GCP_BUCKET_NAME')}/vertex_pipelines/")
-
-# Paramètres à adapter
-
 GCS_INPUT_URI = os.getenv("GCS_INPUT_URI", "gs://bucketllmops/yoda_sentences.csv")
 
 pipeline_package_path = "model_training_pipeline.json"
 
-# Compiler le pipeline
 compiler.Compiler().compile(
 	pipeline_func=model_training_pipeline,
 	package_path=pipeline_package_path
 )
 
-# Initialiser Vertex AI
 aiplatform.init(project=PROJECT_ID, location=REGION)
 
-# Soumettre le pipeline
 job = aiplatform.PipelineJob(
-	display_name="model-training-pipeline",
-	template_path=pipeline_package_path,
-	pipeline_root=PIPELINE_ROOT,
-	parameter_values={
-		"gcs_input_uri": GCS_INPUT_URI,
-	},
-	enable_caching=False,
+    display_name="model-training-pipeline",
+    template_path=pipeline_package_path,
+    pipeline_root=PIPELINE_ROOT,
+    parameter_values={
+        "gcs_input_uri": GCS_INPUT_URI,
+    },
+    enable_caching=False,
 )
-job.run(sync=True)
+
+job.run(service_account="vertex-ai-runner@lunar-byte-475017-r8.iam.gserviceaccount.com", sync=True)
 
 print(f"Pipeline lancé ! Suivez ce lien pour voir l'exécution : {job.dashboard_uri}")
